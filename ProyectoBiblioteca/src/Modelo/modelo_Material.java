00357b329296bb889aaf6e14411d9b81c50e12fa
FelJager <39222331+FelJager@users.noreply.github.com>
2021-07-07 16:25:14 -0500

    actualizacion modelos

--- ProyectoBiblioteca/src/Modelo/modelo_Material.java
+++ ProyectoBiblioteca/src/Modelo/modelo_Material.java
@@ -11,9 +11,9 @@ public class modelo_Material {
 	Date fecha_publicacion;
 	int numero_paginas;
 	String detalle_publicacion;
+	String biblioteca;
 	
-	public modelo_Material(int codigo, String titulo, String[] autores, String tipo_recurso, String editorial, Date fecha_publicacion, int numero_paginas, String detalle_publicacion) 
-	{
+	public modelo_Material(int codigo, String titulo, String[] autores, String tipo_recurso, String editorial, Date fecha_publicacion, int numero_paginas, String detalle_publicacion, String biblioteca) {
 		this.codigo = codigo;
 		this.titulo = titulo;
 		this.autores = autores;
@@ -22,75 +22,65 @@ public class modelo_Material {
 		this.fecha_publicacion = fecha_publicacion;
 		this.numero_paginas = numero_paginas;
 		this.detalle_publicacion = detalle_publicacion;
+		this.biblioteca = biblioteca;
 	}
 	
-	
-
 	public int getCodigo() {
 		return codigo;
 	}
-
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-
 	public String getTitulo() {
 		return titulo;
 	}
-
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
 	public String[] getAutores() {
 		return autores;
 	}
-
 	public void setAutores(String[] autores) {
 		this.autores = autores;
 	}
-
 	public String getTipo_recurso() {
 		return tipo_recurso;
 	}
-
 	public void setTipo_recurso(String tipo_recurso) {
 		this.tipo_recurso = tipo_recurso;
 	}
-
 	public String getEditorial() {
 		return editorial;
 	}
-
 	public void setEditorial(String editorial) {
 		this.editorial = editorial;
 	}
-
 	public Date getFecha_publicacion() {
 		return fecha_publicacion;
 	}
-
 	public void setFecha_publicacion(Date fecha_publicacion) {
 		this.fecha_publicacion = fecha_publicacion;
 	}
-
 	public int getNumero_paginas() {
 		return numero_paginas;
 	}
-
 	public void setNumero_paginas(int numero_paginas) {
 		this.numero_paginas = numero_paginas;
 	}
-
 	public String getDetalle_publicacion() {
 		return detalle_publicacion;
 	}
-
 	public void setDetalle_publicacion(String detalle_publicacion) {
 		this.detalle_publicacion = detalle_publicacion;
 	}
-	
+	public String getBiblioteca() {
+		return biblioteca;
+	}
+	public void setBiblioteca(String biblioteca) {
+		this.biblioteca = biblioteca;
+	}
 	
 	
 	
 }
+
--- ProyectoBiblioteca/src/Modelo/modelo_Usuario.java
+++ ProyectoBiblioteca/src/Modelo/modelo_Usuario.java
@@ -5,11 +5,13 @@ public class modelo_Usuario {
 	String nombres;
 	String apellidos;
 	String carrera;
-	public modelo_Usuario(int codigo_alumno, String nombres, String apellidos, String carrera) {
+	String sucursal;
+	public modelo_Usuario(int codigo_alumno, String nombres, String apellidos, String carrera, String sucursal) {
 		this.codigo_alumno = codigo_alumno;
 		this.nombres = nombres;
 		this.apellidos = apellidos;
 		this.carrera = carrera;
+		this.sucursal = sucursal;
 	}
 	public int getCodigo_alumno() {
 		return codigo_alumno;
@@ -35,7 +37,12 @@ public class modelo_Usuario {
 	public void setCarrera(String carrera) {
 		this.carrera = carrera;
 	}
-	
-	
+	public String getSucursal() {
+		return sucursal;
+	}
+	public void setSucursal(String sucursal) {
+		this.sucursal = sucursal;
+	}
+
 	
 }
